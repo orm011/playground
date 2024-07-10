00779c93b94317897530eb69a8640252c65fa9de
Oscar Moll <orm@csail.mit.edu>
2024-07-10 15:46:42 -0400

    checkpoint proof of concept

--- playground/load.py
+++ playground/load.py
@@ -32,27 +32,26 @@ def transform(image_tuple):
 
 dataset = torchvision.datasets.ImageFolder('./data/objectnet/images', loader=loader, transform=transform)
 def get_image_vectors_iter(dataset):
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=False, num_workers=1)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=20, shuffle=False, num_workers=1)
 
     for batch in dataloader:
         pixel_values = batch[0]['pixel_values'].to(model_device)
         with torch.no_grad():
             image_vectors = clip_model.get_image_features(pixel_values=pixel_values)
-            image_vectors = F.normalize(image_vectors).cpu().numpy()
-
+            # don't normalize for now
+            image_vectors = [vec for vec in image_vectors.cpu().detach().numpy()]
         output_batch = batch[0].copy()
         del output_batch['pixel_values']
         output_batch['image_vectors'] = image_vectors
         yield output_batch
 
 
-def extract_image_vectors():
+def extract_image_vectors(dataset):
     acc = {'label': [], 'path':[], 'image_vectors': []}
     for batch in tqdm(get_image_vectors_iter(dataset)):
         acc['label'].append(batch['label'])
         acc['path'].append(batch['path'])
         acc['image_vectors'].append(batch['image_vectors'])
 
-    df = pd.DataFrame({'label':sum(acc['label'], start=[]), 'path':sum(acc['path'], start=[])})
-    image_vectors = np.concatenate(acc['image_vectors'], axis=0)
-    return df, image_vectors
+    df = pd.DataFrame({'label':sum(acc['label'], start=[]), 'path':sum(acc['path'], start=[]), 'vectors':sum(acc['image_vectors'], start=[])})
+    return df
--- playground/logistic_regression.py
+++ playground/logistic_regression.py
@@ -13,35 +13,27 @@ import math
 
 class LinearModel:
     def __init__(self, *,
-         label_loss_type, reg_norm_lambda,
-         # regularizer_vector,
-         # reg_query_lambda,
-         class_weight,
-         verbose=False, max_iter=100):
+         label_loss_type : str = 'hinge_squared_loss',
+         reg_norm_lambda : float = 10.0, # equiv to C = .1 in sklearn
+         regularizer_vector : np.ndarray | None = None,
+         reg_vector_lambda : float = 1.0,
+         class_weight : str = 'balanced',
+         verbose : bool = False,
+         max_iter : int = 100
+        ):
 
-        # regularizer_vector = torch.from_numpy(regularizer_vector).float()
+        self.regularizer_vector = torch.from_numpy(regularizer_vector).float() if regularizer_vector is not None else None
         assert label_loss_type in ['ce_loss', 'hinge_loss', 'hinge_squared_loss']
         self.label_loss_type = label_loss_type
-        #assert not math.isclose(regularizer_vector.norm().item() , 0.)
-
-        #self.regularizer_vector = F.normalize(regularizer_vector.reshape(-1), dim=-1)
         self._module : nn.Linear | None = None
-        # self.weight = nn.Parameter(self.regularizer_vector.clone(), requires_grad=True)
-        # self.bias = nn.Parameter(torch.tensor(0.), requires_grad=True)
-#        assert not self.weight.isnan().any()
-
         self.max_iter = max_iter
         self.verbose = verbose
-
-#        self.reg_query_lambda = reg_query_lambda
         self.reg_norm_lambda = reg_norm_lambda
-
-        self.class_weight = class_weight #'balanced' #torch.tensor([1.])
+        self.reg_vector_lambda = reg_vector_lambda
+        self.class_weight = class_weight
 
     def _step(self, batch):
         assert self._module
-#        assert not self.weight.isnan().any(), f'{self.weight=}'
-
         X,y=batch # note y can be a floating point
         assert not y.isnan().any()
         assert not X.isnan().any()
@@ -66,26 +58,32 @@ class LinearModel:
             yprime = 2*y - 1 # make it 1, -1
             error_loss = torch.maximum(torch.tensor(0.),  1. - yprime * logits)
             if self.label_loss_type == 'hinge_squared_loss':
-                error_loss = error_loss**2
+                error_loss = .5*error_loss**2
         else:
             assert False, 'unknown loss type'
 
         item_losses = error_loss * point_weights
         norm_squared = self._module.weight.reshape(-1) @ self._module.weight.reshape(-1)
         loss_norm = self.reg_norm_lambda * .5 * norm_squared
+
+        if self.regularizer_vector is not None:
+            loss_regularizer_vector = 1. - F.cosine_similarity(self.regularizer_vector.reshape(1,-1), self._module.weight).sum()
+        else:
+            loss_regularizer_vector = torch.tensor(0.)
+
+        loss_regularizer_vector = self.reg_vector_lambda * loss_regularizer_vector
 #       normalized_weight = F.normalize(self.weight, dim=-1)
 #       loss_norm = self.reg_norm_lambda *  ( torch.cosh( (self.weight @ self.weight).log() ) - 1. )
-#       loss_queryreg = self.reg_query_lambda * ( ( 1 - normalized_weight@self.regularizer_vector )/2. )
         loss_labels = item_losses.sum()
-        total_loss = loss_labels + loss_norm #+ loss_queryreg
+        total_loss = loss_labels + loss_norm + loss_regularizer_vector
 
         ans =  {
             'loss_norm' : loss_norm,
-#         'loss_queryreg': loss_queryreg,
+            'loss_regularizer_vector': loss_regularizer_vector,
             'loss_labels': loss_labels,
             'loss': total_loss,
         }
-        assert not total_loss.isinf() or total_loss.isnan(), f'{total_loss=}'
+        assert not (total_loss.isinf() or total_loss.isnan()), f'{total_loss=}'
         return ans
 
     def training_step(self, batch, batch_idx):
@@ -97,21 +95,15 @@ class LinearModel:
         return losses
 
     def configure_optimizers(self):
-        return opt.LBFGS(self._module.parameters(), max_iter=self.max_iter, tolerance_change=1e-3, line_search_fn='strong_wolfe')
+        return opt.LBFGS(self._module.parameters(), max_iter=self.max_iter, tolerance_change=1e-6, line_search_fn='strong_wolfe')
 
     def fit(self, X, y):
         self._module = nn.Linear(X.shape[1], 1, bias=True)
         trainer_ = BasicTrainer(mod=self, max_epochs=1, verbose=self.verbose)
 
-        ## 1/(nvecs for image)
-        if X.shape[0] > 0:
-            Xmu = X.mean(axis=0)
-            X = X - Xmu.reshape(1,-1) # center this
-            ds = TensorDataset(torch.from_numpy(X), torch.from_numpy(y))
-            dl = DataLoader(ds, batch_size=X.shape[0], shuffle=True)
-        else:
-            dl= None
-
+        assert X.shape[0] > 0
+        ds = TensorDataset(torch.from_numpy(X), torch.from_numpy(y))
+        dl = DataLoader(ds, batch_size=X.shape[0], shuffle=True)
         losses_ = trainer_.fit(dl)
         if self.verbose:
             df = pd.DataFrame.from_records(losses_)
